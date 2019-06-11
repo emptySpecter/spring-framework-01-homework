ddb64e2d7c50a56758ce96eafdbb81edeab25460
emptySpecter <specterknight2005@yandex.com>
2019-06-11 18:57:18 +0300

    initial commit for homework 2

--- src/main/java/ru/otus/spring01/Main.java
+++ src/main/java/ru/otus/spring01/Main.java
@@ -5,10 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring01.service.TestingRunnerService;
 
 public class Main {
-    private static AbstractRefreshableApplicationContext context;
 
     public static void main(String[] args) {
-        context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        AbstractRefreshableApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         TestingRunnerService testingRunnerService = context.getBean(TestingRunnerService.class);
         testingRunnerService.startTesing();
     }
