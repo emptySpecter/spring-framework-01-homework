b8fa693b63d24cc2eb9cd4e18197be86c8788141
EmptySpecter <specterknight2005@yandex.com>
2019-06-28 13:06:40 +0300

    asserts

--- src/test/java/ru/otus/spring01/test/service/StudentServiceImplTest.java
+++ src/test/java/ru/otus/spring01/test/service/StudentServiceImplTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.spring01.service.StudentService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class StudentServiceImplTest {
@@ -16,7 +19,12 @@ public class StudentServiceImplTest {
 
     @Test
     void TestTest() {
-
+        String name = "Mary";
+        assertEquals(1,1);
+        assertTrue(2*2 == 5, "ха-ха");
+        assertEquals(name,"Jane");
+        assertEquals(name,"Mary");
+        assertTrue(2*2 == 3, "ах-ах");
     }
 
 }
