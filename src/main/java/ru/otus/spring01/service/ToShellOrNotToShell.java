a6d1dde01e585767b785693f57dd01b0422a986b
EmptySpecter <specterknight2005@yandex.com>
2019-07-11 16:38:54 +0300

    a little more better

--- src/main/java/ru/otus/spring01/service/ToShellOrNotToShell.java
+++ src/main/java/ru/otus/spring01/service/ToShellOrNotToShell.java
@@ -2,31 +2,24 @@ package ru.otus.spring01.service;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.annotation.Order;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("spring.shell.interactive")
+@ConditionalOnProperty(prefix = "spring.shell.interactive", name = "enabled", havingValue = "false")
 @Order(InteractiveShellApplicationRunner.PRECEDENCE - 100)
 public class ToShellOrNotToShell implements ApplicationRunner {
 
     private final TestingRunnerService testingRunnerService;
-    private boolean shellInteractiveEnabled;
 
     public ToShellOrNotToShell(TestingRunnerService testingRunnerService) {
         this.testingRunnerService = testingRunnerService;
     }
 
-    public void setEnabled(boolean enabled) {
-        shellInteractiveEnabled = enabled;
-    }
-
     @Override
-    public void run(ApplicationArguments args){
-        if (!shellInteractiveEnabled) {
-            testingRunnerService.startTesing();
-        }
+    public void run(ApplicationArguments args) {
+        testingRunnerService.startTesing();
     }
 }
